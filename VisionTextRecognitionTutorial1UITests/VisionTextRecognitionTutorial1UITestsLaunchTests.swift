dc3168a10326a9827bea54bab8c580c706cd38e7
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-01-06 14:23:34 -0500

    Initial Commit

---
+++ VisionTextRecognitionTutorial1UITests/VisionTextRecognitionTutorial1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VisionTextRecognitionTutorial1UITestsLaunchTests.swift
+//  VisionTextRecognitionTutorial1UITests
+//
+//  Created by Caroline LaDouce on 1/6/22.
+//
+
+import XCTest
+
+class VisionTextRecognitionTutorial1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
